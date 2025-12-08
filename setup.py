55a1273ca7466d244da51a0dea386ad189a749c2
Vedant Shivnekar <vedant2308shivenkar@gmail.com>
2025-12-08 15:54:41 +0100

    folder structure updated with init and setup.py is also updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="advanced_rag_document_intelligence_system",
+    version="0.1.0",
+    author="Vedant Shivnekar",
+    author_email="shivnekarvedant2000@gmail.com",
+    description="An advanced RAG document intelligence system.",
+    packages=find_packages(),
+)
